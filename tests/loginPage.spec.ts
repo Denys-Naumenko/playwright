18c8e1b6183680083cfee1c4267de81e38671d0a
Denys Naumenko <d.naumenko1997@gmail.com>
2024-01-03 00:04:58 +0200

    fix test name

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -2,17 +2,17 @@ import { test, expect } from '@playwright/test';
 import HomePage from '../pages/homePage';
 import LoginPage from '../pages/loginPage';
 
-test.describe('Open Home page in browser and validate visible categories', async () => {
+test.describe('Test for Login Page', async () => {
   let page: Page;
     let loginPage;
 
-  test.beforeAll('Visit homepage', async ({ browser }) => {
+  test.beforeAll('Visit login page', async ({ browser }) => {
     page = await browser.newPage({ waitUntil: "domcontentloaded" });
     loginPage = new LoginPage(page);
     await loginPage.visitLoginPage();
   });
 
-    test("Validate available categories", async () => {
+    test("Type user data, login and validate login", async () => {
          
         await loginPage.typeUserName();
         await loginPage.typePassword();
